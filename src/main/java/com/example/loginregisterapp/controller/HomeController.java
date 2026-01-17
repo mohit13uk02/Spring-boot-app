865899f4a095ddd06e1ed0c9db99aabcdcc39b67
mohit13uk02 <tamtamohit814@gmail.com>
2026-01-17 20:56:54 +0530

    Update HomeController.java

--- src/main/java/com/example/loginregisterapp/controller/HomeController.java
+++ src/main/java/com/example/loginregisterapp/controller/HomeController.java
@@ -1,5 +1,4 @@
 package com.example.loginregisterapp.controller;
-
 import com.example.loginregisterapp.model.User;
 import com.example.loginregisterapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +11,20 @@ public class HomeController {
 
     @Autowired
     private UserRepository userRepository;
-
-    // Register Page
     @GetMapping("/register")
     public String showRegisterPage(Model model) {
         model.addAttribute("user", new User());
         return "register";
     }
-
-    // Save User
     @PostMapping("/register")
     public String registerUser(@ModelAttribute User user) {
         userRepository.save(user);
         return "redirect:/login";
     }
-
-    // Login Page
     @GetMapping("/login")
     public String showLoginPage() {
         return "login";
     }
-
-    // Login Logic
     @PostMapping("/login")
     public String loginUser(@RequestParam String email,
                             @RequestParam String password,
