2f8033f5a6b9b4a47ecf269f18293f971c9112bf
mohit13uk02 <tamtamohit814@gmail.com>
2026-01-17 20:59:27 +0530

    Update LoginRegisterApplication.java

--- src/main/java/com/example/loginregisterapp/LoginRegisterApplication.java
+++ src/main/java/com/example/loginregisterapp/LoginRegisterApplication.java
@@ -1,8 +1,6 @@
 package com.example.loginregisterapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class LoginRegisterApplication {
 
