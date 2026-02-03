b834ce63dd6a2fd144f5eb1e8f1157311eb101ec
mohit13uk02 <tamtamohit814@gmail.com>
2026-02-03 08:40:08 +0530

    Update HomeController.java

--- src/main/java/com/example/loginregisterapp/controller/HomeController.java
+++ src/main/java/com/example/loginregisterapp/controller/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+
 
 @Controller
 public class HomeController {
