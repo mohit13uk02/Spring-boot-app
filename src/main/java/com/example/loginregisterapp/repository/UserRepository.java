80e254b74102f350514c1efeabc2b1e137f2286c
mohit13uk02 <tamtamohit814@gmail.com>
2026-02-04 14:41:00 +0530

    Update UserRepository.java

--- src/main/java/com/example/loginregisterapp/repository/UserRepository.java
+++ src/main/java/com/example/loginregisterapp/repository/UserRepository.java
@@ -2,5 +2,6 @@ package com.example.loginregisterapp.repository;
 import com.example.loginregisterapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
+  boolean existsByEmail(String email);
 User findByEmailAndPassword(String email, String password);
 }
